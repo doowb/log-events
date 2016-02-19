92cb5a50c61e243182d195faa9b421a8eab66102
Brian Woodward <brian.woodward@gmail.com>
2016-02-19 13:40:50 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * log-events <https://github.com/doowb/log-events>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function() {
+};
+
